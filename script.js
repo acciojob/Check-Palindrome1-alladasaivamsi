a38d1a2abc474503b0da5b3a594e52101c3cc295
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-03-20 21:08:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
+	let s = 0 ,  e = str.length - 1;
+	str = str.toLowerCase();
+	while(s <= e)
+		{
+			if(str.charAt(s) === str.charAt(e))
+			{
+				s++;
+				e--;
+			}
 
+			else
+			{
+				return false;
+			}
+		}
+	return true;
 }
 module.exports = palindrome
